5a097e9045fcf2c124463ef26f4991a7e885b3c1
Simone <simone.riva@bluewin.ch>
2021-12-07 21:58:21 +0100

    Letter Box

--- letterbox/src/letterbox_fun.rs
+++ letterbox/src/letterbox_fun.rs
@@ -1,6 +1,5 @@
 use regex::Regex;
 use std::any::Any;
-use std::collections::BTreeMap;
 use std::collections::{HashMap, HashSet};
 use std::fs;
 
@@ -46,10 +45,7 @@ impl Dispatcher for DataStorageManager {
                 return Box::new("");
             }
         } else if message.message == String::from("words") {
-            let bv = Box::new(self.data.split_whitespace().fold(Vec::new(), |mut vs, el| {
-                vs.push(el.to_string());
-                vs
-            }));
+            let bv = Box::new(self.words());
             return bv;
         }
 
@@ -91,9 +87,103 @@ pub struct StopWordManager {
     stop_words: HashSet<String>,
 }
 
+impl StopWordManager {
+    fn new() -> Self {
+        StopWordManager {
+            stop_words: HashSet::new(),
+        }
+    }
+
+    fn init(&mut self, file_stop_w: &String) -> Box<dyn Any> {
+        self.stop_words = fs::read_to_string(file_stop_w)
+            .expect("Fail in read stop words")
+            .to_string()
+            .split(",")
+            .fold(HashSet::new(), |mut hs, el| {
+                hs.insert(el.to_string());
+                hs
+            });
+
+        Box::new(true)
+    }
+
+    fn is_stop_word(&mut self, word: &String) -> Box<dyn Any> {
+        Box::new(self.stop_words.contains(word))
+    }
+}
+
+impl Dispatcher for StopWordManager {
+    fn dispatch(&mut self, message: &MessageLB) -> Box<dyn Any> {
+        let msg_data = match message.msg_data.downcast_ref::<String>() {
+            Some(str) => String::from(str),
+            None => {
+                assert!(false);
+                String::from("")
+            }
+        };
+
+        if message.message == "init" {
+            self.init(&msg_data)
+        } else if message.message == "is_stop_word" {
+            self.is_stop_word(&msg_data)
+        } else {
+            assert!(false);
+            Box::new(false)
+        }
+    }
+}
+
 //////////////////////////////////////////////////////////////////
 pub struct WordFrequencyManager {
     word_freqs: HashMap<String, i32>,
+}
+
+impl Dispatcher for WordFrequencyManager {
+    fn dispatch(&mut self, message: &MessageLB) -> Box<dyn Any> {
+        if message.message == "increment_count" {
+            let msg_data = match message.msg_data.downcast_ref::<String>() {
+                Some(str) => String::from(str),
+                None => {
+                    assert!(false);
+                    String::from("")
+                }
+            };
+
+            self.increment_count(&msg_data);
+            Box::new(true)
+        } else if message.message == "sorted" {
+            self.sorted()
+        } else {
+            assert!(false);
+            Box::new(false)
+        }
+    }
+}
+
+impl WordFrequencyManager {
+    fn new() -> Self {
+        WordFrequencyManager {
+            word_freqs: HashMap::new(),
+        }
+    }
+
+    fn increment_count(&mut self, word: &String) {
+        *self.word_freqs.entry(word.clone()).or_insert(0) += 1;
+    }
+
+    fn sorted(&mut self) -> Box<dyn Any> {
+        let mut ves: Vec<(String, i32)> = Vec::new();
+        ves.reserve(self.word_freqs.len());
+
+        for (st, freq) in &self.word_freqs {
+            if *freq > 4 {
+                ves.push((st.clone(), freq.clone()));
+            }
+        }
+
+        ves.sort_by_key(|el| -el.1);
+        Box::new(ves)
+    }
 }
 
 //////////////////////////////////////////////////////////////////
@@ -103,24 +193,95 @@ struct WordFrequencyController {
     word_freq_manager: WordFrequencyManager,
 }
 
-pub fn letterbox_test(file_name: &String, file_stop_w: &String) {
-    let mut dsm: DataStorageManager = DataStorageManager::new();
+impl Dispatcher for WordFrequencyController {
+    fn dispatch(&mut self, message: &MessageLB) -> Box<dyn Any> {
+        if message.message == "init" {
+            let msg_data = match message.msg_data.downcast_ref::<Vec<String>>() {
+                Some(vs) => vs.clone(),
+                None => {
+                    assert!(false);
+                    Vec::new() as Vec<String>
+                }
+            };
 
-    let  msg = MessageLB::new(&String::from("init"), Box::new(file_name.clone()));
-
-    let  rr = dsm.dispatch(&msg);
-
-    let text = rr.downcast_ref::<String>().unwrap().clone();
-
-    let  msg_2 = MessageLB::new(&String::from("words"), Box::new(""));
-
-    println!("{}", text);
-
-    let  rr_2 = dsm.dispatch(&msg_2);
-
-    let vs = rr_2.downcast_ref::<Vec<String>>().unwrap().clone();
-
-    for el in vs.iter() {
-        println!("{}", el);
+            self.init(&msg_data[0], &msg_data[1]);
+            Box::new(true)
+        } else if message.message == "run" {
+            self.run()
+        } else {
+            assert!(false);
+            Box::new(false)
+        }
     }
+}
+
+impl WordFrequencyController {
+    fn new() -> Self {
+        WordFrequencyController {
+            stop_word_manager: StopWordManager::new(),
+            storage_manager: DataStorageManager::new(),
+            word_freq_manager: WordFrequencyManager::new(),
+        }
+    }
+
+    fn init(&mut self, file_name: &String, file_stop_w: &String) {
+        let msg_sm = MessageLB::new(&"init".to_string(), Box::new(file_name.to_string()));
+        self.storage_manager.dispatch(&msg_sm);
+
+        let msg_sw = MessageLB::new(&"init".to_string(), Box::new(file_stop_w.to_string()));
+        self.stop_word_manager.dispatch(&msg_sw);
+    }
+
+    fn run(&mut self) -> Box<dyn Any> {
+        let msg_words = MessageLB::new(&"words".to_string(), Box::new(false));
+        let box_vs = self.storage_manager.dispatch(&msg_words);
+
+        for w in box_vs.downcast_ref::<Vec<String>>().unwrap().iter() {
+            let msg_stop_w = MessageLB::new(&"is_stop_word".to_string(), Box::new(w.clone()));
+
+            if !*self
+                .stop_word_manager
+                .dispatch(&msg_stop_w)
+                .downcast_ref::<bool>()
+                .unwrap()
+            {
+                let msg_freq_manager =
+                    MessageLB::new(&"increment_count".to_string(), Box::new(w.clone()));
+                self.word_freq_manager.dispatch(&msg_freq_manager);
+            }
+        }
+
+        let msg_sorted = MessageLB::new(&"sorted".to_string(), Box::new(true));
+        let res_str = self
+            .word_freq_manager
+            .dispatch(&msg_sorted)
+            .downcast_ref::<Vec<(String, i32)>>()
+            .unwrap()
+            .iter()
+            .fold(String::from(""), |mut ss, el| {
+                ss.push_str(&format!("{} - {}\n", el.0, el.1));
+                ss
+            });
+
+        Box::new(res_str)
+    }
+}
+
+pub fn letterbox_test(file_name: &String, file_stop_w: &String) {
+    let mut wfc = WordFrequencyController::new();
+
+    let msg_init = MessageLB::new(
+        &"init".to_string(),
+        Box::new(vec![file_name.clone(), file_stop_w.clone()]),
+    );
+    let msg_run = MessageLB::new(&"run".to_string(), Box::new(true));
+
+    wfc.dispatch(&msg_init);
+    let res = wfc
+        .dispatch(&msg_run)
+        .downcast_ref::<String>()
+        .unwrap()
+        .clone();
+
+    println!("{}", res);
 }
