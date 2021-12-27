145cd23b268a8151214b6a483529d22c3a418931
Simone <simone.riva@bluewin.ch>
2021-12-27 21:19:49 +0100

    lazy

---
+++ lazy_rivers/src/lazy_rivers_fun.rs
@@ -0,0 +1,12 @@
+use regex::Regex;
+use std::any::Any;
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::str::Chars;
+
+// fn read_text(file_name: &String) -> impl Chars {
+//     let text =fs::read_to_string(file_name).expect("Error");
+//     text.chars()
+// }
+
+pub fn lazy_rivers_test(file_name: &String, file_stop_w: &String) {}
---
+++ lazy_rivers/src/lib.rs
@@ -0,0 +1,3 @@
+mod lazy_rivers_fun;
+
+pub use crate::lazy_rivers_fun::lazy_rivers_test;
