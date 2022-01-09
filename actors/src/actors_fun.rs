1586b1ecc480e277a70366a6b456c440bd5da905
Simone <simone.riva@bluewin.ch>
2022-01-09 11:25:13 +0100

    actors 0.0.0.2

--- actors/src/actors_fun.rs
+++ actors/src/actors_fun.rs
@@ -1,19 +1,13 @@
 use regex::Regex;
 use std::any::Any;
-use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::hash::Hash;
 use std::rc::Rc;
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
-
-macro_rules! make_rc_refcell {
-    ($value:expr) => {
-        Rc::new(RefCell::new($value))
-    };
-}
 
 macro_rules! make_arc_mutex {
     ($value:expr) => {
@@ -21,21 +15,33 @@ macro_rules! make_arc_mutex {
     };
 }
 
-type RcRefAny = Rc<RefCell<dyn Any>>;
-struct MessageAct {
+pub struct MessageStringAct {
     message: String,
-    msg_data: RcRefAny,
+    msg_data: String,
 }
 
+impl MessageStringAct {
+    fn new(message: &String, msg_data: &String) -> Self {
+        MessageStringAct {
+            message: message.clone(),
+            msg_data: msg_data.clone(),
+        }
+    }
+}
+
+///////////////////////////////////////////////
 pub struct DataStorageManager {
     data: String,
-    recv: Arc<Mutex<Receiver<MessageAct>>>,
-    send: Sender<MessageAct>,
+    recv: Arc<Mutex<Receiver<MessageStringAct>>>,
+    pub send: Sender<MessageStringAct>,
+    // send_to_next: Sender<MessageStringAct>,
 }
+
+type ArcMutexDataStorageManager = Arc<Mutex<DataStorageManager>>;
 
 impl DataStorageManager {
     pub fn new() -> Self {
-        let (txm, rxm) = mpsc::channel::<MessageAct>();
+        let (txm, rxm) = mpsc::channel::<MessageStringAct>();
         let rx_arcm = make_arc_mutex!(rxm);
 
         DataStorageManager {
@@ -44,35 +50,84 @@ impl DataStorageManager {
             send: txm.clone(),
         }
     }
+
+    pub fn new_data_storage_listener() -> (
+        ArcMutexDataStorageManager,
+        Sender<MessageStringAct>,
+        JoinHandle<()>,
+    ) {
+        let dsm_l = make_arc_mutex!(DataStorageManager::new());
+        let dsm_send = dsm_l.lock().unwrap().send.clone();
+
+        let dsm_l_clone = dsm_l.clone();
+
+        let join = thread::spawn(move || {
+            dsm_l_clone.lock().ok().unwrap().dispatch();
+        });
+
+        (dsm_l, dsm_send, join)
+    }
+
+    fn init(&mut self, file_name: &String) {
+        let data = Regex::new(r"[\W_]+")
+            .unwrap()
+            .replace_all(
+                &fs::read_to_string(file_name)
+                    .expect("some error in read data")
+                    .to_lowercase(),
+                " ",
+            )
+            .to_string();
+
+        println!("{}", data);
+        self.data = data;
+    }
+
+    fn dispatch(&mut self) {
+        for msg in self.recv.clone().lock().ok().unwrap().iter() {
+            if msg.message == "kill" {
+                break;
+            } else if msg.message == "init" {
+                let file_name = msg.msg_data.clone();
+                self.init(&file_name);
+            }
+        }
+    }
 }
 
-fn recv(rr: Arc<Mutex<Receiver<i32>>>) -> JoinHandle<()> {
-    let j = thread::spawn(move || {
-        for xx in rr.clone().borrow_mut().lock().ok().unwrap().iter() {
-            println!("{}", xx);
-        }
-    });
+//////////////////////////////////////////////////
+struct StopWordsManager {
+    stop_words: HashSet<String>,
+}
 
-    j
+impl StopWordsManager {
+    fn new() -> Self {
+        StopWordsManager {
+            stop_words: HashSet::new(),
+        }
+    }
+
+    fn init(&mut self, file_stop_w: &String) {
+        self.stop_words = fs::read_to_string(file_stop_w)
+            .expect("error SW")
+            .split(",")
+            .map(|el| String::from(el))
+            .collect::<HashSet<_>>();
+    }
 }
 
 pub fn actors_test(file_name: &String, file_stop_w: &String) {
+    let (_dsm_l, send_dsm, j_dsm) = DataStorageManager::new_data_storage_listener();
 
+    let _res = send_dsm.send(MessageStringAct::new(
+        &String::from("init"),
+        &String::from(file_name),
+    ));
 
-    let mut dsm = DataStorageManager::new();
+    let _res = send_dsm.send(MessageStringAct::new(
+        &String::from("kill"),
+        &String::from(file_name),
+    ));
 
-    let (tx, rx) = mpsc::channel::<i32>();
-    let (txm, rxm) = mpsc::channel::<MessageAct>();
-
-    let arc_rx = Arc::new(Mutex::new(rx));
-
-    let arc_rxm = make_arc_mutex!(rxm);
-
-    let j = recv(arc_rx);
-
-    for i in 1..100 {
-        tx.send(i);
-    }
-
-    j.join();
+    j_dsm.join();
 }
