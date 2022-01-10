f71d0a58a6ac0cae74fa3480e0daf3351a8fbd44
Simone <simone.riva@bluewin.ch>
2022-01-10 21:45:29 +0100

    actor 0.0.0.2

--- actors/src/actors_fun.rs
+++ actors/src/actors_fun.rs
@@ -14,6 +14,7 @@ macro_rules! make_arc_mutex {
 enum MessageData {
     TextData(String),
     WordData(String),
+    FreqData((String, i32)),
     Eof,
     Empty,
     Kill,
@@ -22,6 +23,23 @@ enum MessageData {
 pub struct MessageAct {
     message: String,
     msg_data: MessageData,
+}
+
+macro_rules! make_act_channel {
+    () => {{
+        let (send_msg, recv_msg) = mpsc::channel::<MessageAct>();
+        let arc_mutex_recv_msg = make_arc_mutex!(recv_msg);
+        (send_msg, arc_mutex_recv_msg)
+    }};
+}
+
+macro_rules! freq_message {
+    ($message:expr, $message_data:expr) => {
+        MessageAct {
+            message: $message.clone(),
+            msg_data: MessageData::FreqData($message_data),
+        }
+    };
 }
 
 macro_rules! text_message {
@@ -69,47 +87,41 @@ macro_rules! kill_message {
     };
 }
 
-impl MessageAct {
-    fn new(message: &String, msg_data: MessageData) -> Self {
-        MessageAct {
-            message: message.clone(),
-            msg_data: msg_data,
-        }
-    }
-}
+// impl MessageAct {
+//     fn new(message: &String, msg_data: MessageData) -> Self {
+//         MessageAct {
+//             message: message.clone(),
+//             msg_data: msg_data,
+//         }
+//     }
+// }
 
 ///////////////////////////////////////////////
 pub struct DataStorageManager {
     data: String,
     recv: Arc<Mutex<Receiver<MessageAct>>>,
-    pub send: Sender<MessageAct>,
     send_to_filter: Sender<MessageAct>,
 }
 
 type ArcMutexDataStorageManager = Arc<Mutex<DataStorageManager>>;
 
 impl DataStorageManager {
-    pub fn new(send_to_filter: &Sender<MessageAct>) -> Self {
-        let (txm, rxm) = mpsc::channel::<MessageAct>();
-        let rx_arcm = make_arc_mutex!(rxm);
-
+    pub fn new(
+        receiver: &Arc<Mutex<Receiver<MessageAct>>>,
+        send_to_filter: &Sender<MessageAct>,
+    ) -> Self {
         DataStorageManager {
             data: String::new(),
-            recv: rx_arcm.clone(),
-            send: txm.clone(),
+            recv: receiver.clone(),
             send_to_filter: send_to_filter.clone(),
         }
     }
 
     pub fn new_data_storage_listener(
+        receiver: &Arc<Mutex<Receiver<MessageAct>>>,
         send_to_filter: &Sender<MessageAct>,
-    ) -> (
-        ArcMutexDataStorageManager,
-        Sender<MessageAct>,
-        JoinHandle<()>,
-    ) {
-        let dsm_l = make_arc_mutex!(DataStorageManager::new(send_to_filter));
-        let dsm_send = dsm_l.lock().unwrap().send.clone();
+    ) -> (ArcMutexDataStorageManager, JoinHandle<()>) {
+        let dsm_l = make_arc_mutex!(DataStorageManager::new(receiver, send_to_filter));
 
         let dsm_l_clone = dsm_l.clone();
 
@@ -117,7 +129,7 @@ impl DataStorageManager {
             dsm_l_clone.lock().ok().unwrap().dispatch();
         });
 
-        (dsm_l, dsm_send, join)
+        (dsm_l, join)
     }
 
     fn init(&mut self, file_name: &String) {
@@ -131,7 +143,7 @@ impl DataStorageManager {
             )
             .to_string();
 
-        println!("{}", data);
+        // println!("{}", data);
         self.data = data;
     }
 
@@ -169,12 +181,64 @@ impl DataStorageManager {
 //////////////////////////////////////////////////
 struct StopWordsManager {
     stop_words: HashSet<String>,
+    recv: Arc<Mutex<Receiver<MessageAct>>>,
+    send_to_next: Sender<MessageAct>,
 }
 
+type ArcMutexStopWordsManager = Arc<Mutex<StopWordsManager>>;
+
 impl StopWordsManager {
-    fn new() -> Self {
+    fn new(recv: &Arc<Mutex<Receiver<MessageAct>>>, send_to_next: &Sender<MessageAct>) -> Self {
         StopWordsManager {
             stop_words: HashSet::new(),
+            recv: recv.clone(),
+            send_to_next: send_to_next.clone(),
+        }
+    }
+
+    fn new_stop_words_listener(
+        recv: &Arc<Mutex<Receiver<MessageAct>>>,
+        send_to_next: &Sender<MessageAct>,
+    ) -> (ArcMutexStopWordsManager, JoinHandle<()>) {
+        let sw_l = make_arc_mutex!(StopWordsManager::new(recv, send_to_next));
+
+        let sw_l_clone = sw_l.clone();
+
+        let join = thread::spawn(move || {
+            sw_l_clone.lock().ok().unwrap().dispatch();
+        });
+
+        (sw_l, join)
+    }
+
+    fn dispatch(&mut self) {
+        for msg in self.recv.clone().lock().ok().unwrap().iter() {
+            if msg.message == "kill" {
+                break;
+            } else if msg.message == "init" {
+                let file_name = match msg.msg_data {
+                    MessageData::TextData(s) => String::from(s),
+                    _ => {
+                        assert!(false, "some error in SW init message");
+                        String::new()
+                    }
+                };
+                self.init(&file_name);
+            } else if msg.message == "filter" {
+                let word = match msg.msg_data {
+                    MessageData::WordData(s) => String::from(s),
+                    MessageData::Eof => String::new(),
+                    _ => {
+                        assert!(false, "some error in SW filter message");
+                        String::new()
+                    }
+                };
+                if word.len() > 0 {
+                    self.filter(&word);
+                } else {
+                    let _ = self.send_to_next.send(eof_message!(String::from("word")));
+                }
+            }
         }
     }
 
@@ -187,18 +251,102 @@ impl StopWordsManager {
     }
 
     fn filter(&self, word: &String) {
-        if !self.stop_words.contains(word) && word.len() > 1 {}
+        if !self.stop_words.contains(word) && word.len() > 1 {
+            // println!("filter: {}", word);
+            let _ = self
+                .send_to_next
+                .send(word_message!(String::from("word"), word));
+        }
+    }
+}
+
+//////////////////////////////////////////////////
+struct FrequenciesManager {
+    frequencies: HashMap<String, i32>,
+    recv: Arc<Mutex<Receiver<MessageAct>>>,
+    send_to_next: Sender<MessageAct>,
+}
+
+type ArcMutexFrequenciesManager = Arc<Mutex<FrequenciesManager>>;
+
+impl FrequenciesManager {
+    fn new(recv: &Arc<Mutex<Receiver<MessageAct>>>, send_to_next: &Sender<MessageAct>) -> Self {
+        FrequenciesManager {
+            frequencies: HashMap::new(),
+            recv: recv.clone(),
+            send_to_next: send_to_next.clone(),
+        }
+    }
+
+    fn new_frequencies_listener(
+        recv: &Arc<Mutex<Receiver<MessageAct>>>,
+        send_to_next: &Sender<MessageAct>,
+    ) -> (ArcMutexFrequenciesManager, JoinHandle<()>) {
+        let fr_l = make_arc_mutex!(FrequenciesManager::new(recv, send_to_next));
+
+        let fr_l_clone = fr_l.clone();
+
+        let join = thread::spawn(move || {
+            fr_l_clone.lock().ok().unwrap().dispatch();
+        });
+
+        (fr_l, join)
+    }
+
+    fn dispatch(&mut self) {
+        for msg in self.recv.clone().lock().ok().unwrap().iter() {
+            if msg.message == "kill" {
+                break;
+            } else if msg.message == "word" {
+                match msg.msg_data {
+                    MessageData::WordData(word) => {
+                        self.update_freq(&word);
+                    }
+                    MessageData::Eof => {
+                        self.send_frequencies();
+                    }
+                    _ => {
+                        assert!(false, "some error in Freq message");
+                    }
+                };
+            }
+        }
+    }
+
+    fn update_freq(&mut self, word: &String) {
+        *self.frequencies.entry(word.clone()).or_insert(0) += 1;
+    }
+
+    fn send_frequencies(&self) {
+        for pair in self.frequencies.iter() {
+            println!("send freq {} - {}", pair.0, pair.1);
+            let _ = self.send_to_next.send(freq_message!(
+                String::from("freq"),
+                (pair.0.clone(), pair.1.clone())
+            ));
+        }
+        let _ = self.send_to_next.send(eof_message!(String::from("freq")));
     }
 }
 
 pub fn actors_test(file_name: &String, file_stop_w: &String) {
-    let (tx, rx) = mpsc::channel::<MessageAct>();
+    let (send_dsm, recv_dsm) = make_act_channel!();
+    let (send_sw_filter, recv_sw_filter) = make_act_channel!();
+    let (send_freq, recv_freq) = make_act_channel!();
+    let (send_sort, recv_sort) = make_act_channel!();
 
-    let (_dsm_l, send_dsm, j_dsm) = DataStorageManager::new_data_storage_listener(&tx);
+    let (_dsm_l, j_dsm) = DataStorageManager::new_data_storage_listener(&recv_dsm, &send_sw_filter);
+    let (_sw_l, j_sw) = StopWordsManager::new_stop_words_listener(&recv_sw_filter, &send_freq);
+    let (_fr_l, j_fr) = FrequenciesManager::new_frequencies_listener(&recv_freq, &send_sort);
 
     let _res = send_dsm.send(text_message!(String::from("init"), file_name));
+    let _res = send_sw_filter.send(text_message!(String::from("init"), file_stop_w));
+
     let _res = send_dsm.send(empty_message!(String::from("send_words")));
-    let _res = send_dsm.send(kill_message!());
+    // let _res = send_dsm.send(kill_message!());
+    // let _res = send_sw_filter.send(kill_message!());
 
     j_dsm.join();
+    j_sw.join();
+    j_fr.join();
 }
